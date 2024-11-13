ced4ab5b3b029098ee8d85e270f65e897fe89c33
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2024-11-13 20:50:37 -0300

    show context menu on image click

---
+++ src/components/ContextMenu.jsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import styles from "./ContextMenu.module.css";
+import Character from "./Character";
+
+const ContextMenu = ({ items, position, onSelect }) => {
+  return (
+    <div
+      className={styles.contextMenu}
+      style={{
+        top: `${position.y}px`,
+        left: `${position.x}px`,
+      }}
+    >
+      <ul>
+        {items.map((item) => (
+          <Character key={item.id} character={item} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+ContextMenu.propTypes = {
+  items: PropTypes.array.isRequired,
+  position: PropTypes.shape({
+    x: PropTypes.number.isRequired,
+    y: PropTypes.number.isRequired,
+  }).isRequired,
+  visible: PropTypes.bool.isRequired,
+  onSelect: PropTypes.func.isRequired,
+};
+
+export default ContextMenu;
--- src/views/Playing.jsx
+++ src/views/Playing.jsx
@@ -1,17 +1,64 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import GameContext from "../contexts/GameContext";
 import PropTypes from "prop-types";
 import Header from "../components/Header";
 import styles from "./Playing.module.css";
+import ContextMenu from "../components/ContextMenu";
 
 const Playing = ({ setView }) => {
   const { selectedCharacters, gameTimer } = useContext(GameContext);
+
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [menuPosition, setMenuPosition] = useState({ x: 0, y: 0 });
+  const [normalizedCoords, setNormalizedCoords] = useState({ x: 0, y: 0 });
+
+  const handleImageClick = (e) => {
+    const image = e.target.getBoundingClientRect();
+
+    const CORD_X = e.clientX - image.x;
+    const CORD_Y = e.clientY - image.y;
+
+    console.log(CORD_X);
+    if (menuVisible) setMenuVisible(false);
+    else {
+      setMenuVisible(true);
+      setMenuPosition({ x: CORD_X + 5, y: CORD_Y + 145 });
+
+      normalizeCoords(image, CORD_X, CORD_Y);
+    }
+  };
+
+  function normalizeCoords(image, x, y) {
+    const newX = x / image.width;
+    const newY = y / image.height;
+
+    setNormalizedCoords({ x: newX, y: newY });
+  }
+
+  const handleCharacterSelect = (character) => {
+    alert(
+      `Selected character: ${character.name}, at coords: ${normalizedCoords}`
+    );
+    setMenuVisible(false);
+  };
 
   return (
     <>
       <Header selectedCharacters={selectedCharacters} gameTimer={gameTimer} />
       <main className={styles.container}>
-        <img className={styles.image} src="/island.png" alt="" />
+        <img
+          onClick={(e) => handleImageClick(e)}
+          className={styles.image}
+          src="/island.png"
+          alt=""
+        />
+        {menuVisible && (
+          <ContextMenu
+            items={selectedCharacters}
+            position={menuPosition}
+            onSelect={handleCharacterSelect}
+          />
+        )}
       </main>
     </>
   );
